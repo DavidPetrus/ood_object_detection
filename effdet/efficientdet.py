ec18573801d79fac4439b9ca0ac549eb144fcc05
DavidPetrus <davidpetrus94@gmail.com>
2021-02-14 14:29:03 +0200

    Add correct padding.

--- effdet/efficientdet.py
+++ effdet/efficientdet.py
@@ -634,6 +634,7 @@ class MetaHead(nn.Module):
             bn_w_lev = bn_rep_w[level*self.num_layers:(level+1)*self.num_layers]
             bn_b_lev = bn_rep_b[level*self.num_layers:(level+1)*self.num_layers]
             for conv_dw,conv_pw,conv_pb,bn_w,bn_b in zip(conv_dw_rep,conv_pw_rep,conv_pb_rep,bn_w_lev,bn_b_lev):
+                x_level = F.pad(x_level,(1,1,1,1))
                 x_level = F.conv2d(x_level, conv_dw, groups=conv_dw.shape[0], padding=(0,0))
                 x_level = F.conv2d(x_level, conv_pw, conv_pb)
                 x_level = F.batch_norm(x_level,self.running_mu,self.running_std,bn_w,bn_b,training=True)
